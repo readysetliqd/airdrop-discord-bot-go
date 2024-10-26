b1dddaa7d4af915548a4da113a55fccf748def48
readyset-liqd <135415819+readysetliqd@users.noreply.github.com>
2024-10-26 16:14:24 +0700

    change logic reading from config file to reading from environment variables

--- config/config.go
+++ config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 )
@@ -19,54 +18,21 @@ var (
 	BotOperatorRoleID   string
 	TwitterEmoji        string
 	TwitterEmojiName    string
-
-	config *Config
 )
 
-type Config struct {
-	Token               string `json:"token"`
-	BotPrefix           string `json:"botPrefix"`
-	DefaultChannelID    string `json:"channelID"`
-	GuildID             string `json:"guildID"`
-	FundingRoundRoleID  string `json:"fundingRoundRoleID"`
-	EarlyRoundRoleID    string `json:"earlyRoundRoleID"`
-	BinanceRoundRoleID  string `json:"binanceRoundRoleID"`
-	ParadigmRoundRoleID string `json:"paradigmRoundRoleID"`
-	CoinbaseRoundRoleID string `json:"coinbaseRoundRoleID"`
-	BotOperatorRoleID   string `json:"botOperatorRoleID"`
-	TwitterEmojiName    string `json:"twitterEmojiName"`
-	TwitterEmojiID      string `json:"twitterEmojiID"`
-}
-
-// ReadConfig reads the config.json file and unmarshals it into the Config struct
+// ReadConfig reads the environment variables and assigns them into global exported variables
 func ReadConfig() error {
-	// read config file in entirety
-	fmt.Println("Reading config.json...")
-	file, err := os.ReadFile("./config.json")
-	if err != nil {
-		return err
-	}
-
-	// unmarshal file into config struct
-	fmt.Println("Unmarshalling config.json...")
-	err = json.Unmarshal(file, &config)
-	if err != nil {
-		fmt.Println("Error unmarshalling config.json")
-		return err
-	}
-
-	Token = config.Token
-	BotPrefix = config.BotPrefix
-	DefaultChannelID = config.DefaultChannelID
-	GuildID = config.GuildID
-	//TODO: change all these two fmt.Sprintf()
-	FundingRoundRoleID = "<@&" + config.FundingRoundRoleID + ">"
-	EarlyRoundRoleID = "<@&" + config.EarlyRoundRoleID + ">"
-	BinanceRoundRoleID = "<@&" + config.BinanceRoundRoleID + ">"
-	ParadigmRoundRoleID = "<@&" + config.ParadigmRoundRoleID + ">"
-	CoinbaseRoundRoleID = "<@&" + config.CoinbaseRoundRoleID + ">"
-	BotOperatorRoleID = config.BotOperatorRoleID
-	TwitterEmoji = fmt.Sprintf("%s:%s", config.TwitterEmojiName, config.TwitterEmojiID)
-	TwitterEmojiName = config.TwitterEmojiName
+	Token = os.Getenv("token")
+	BotPrefix = os.Getenv("botPrefix")
+	DefaultChannelID = os.Getenv("channelID")
+	GuildID = os.Getenv("guildID")
+	FundingRoundRoleID = fmt.Sprintf("<@&%s>", os.Getenv("fundingRoundRoleID"))
+	EarlyRoundRoleID = fmt.Sprintf("<@&%s>", os.Getenv("earlyRoundRoleID"))
+	BinanceRoundRoleID = fmt.Sprintf("<@&%s>", os.Getenv("binanceRoundRoleID"))
+	ParadigmRoundRoleID = fmt.Sprintf("<@&%s>", os.Getenv("paradigmRoundRoleID"))
+	CoinbaseRoundRoleID = fmt.Sprintf("<@&%s>", os.Getenv("coinbaseRoundRoleID"))
+	BotOperatorRoleID = os.Getenv("botOperatorRoleID")
+	TwitterEmoji = fmt.Sprintf("%s:%s", os.Getenv("twitterEmojiName"), os.Getenv("twitterEmojiID"))
+	TwitterEmojiName = os.Getenv("twitterEmojiName")
 	return nil
 }
